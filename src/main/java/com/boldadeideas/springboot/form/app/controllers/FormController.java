0c71a46a2f839c84ca2f6fc523446b60b858e572
hecto <hecto@LAPTOP-QH1AMIIM>
2022-10-09 16:16:14 -0600

    Clase 53 - Validacion del formulacio usando anotaciones

--- src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
@@ -1,7 +1,13 @@
 package com.boldadeideas.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,9 +27,22 @@ public class FormController {
 	// You can use @RequestParam to get an attribute from the form with the name
 	// @RequestParam String username, @RequestParam String password, @RequestParam
 	// String email
-	public String procesar(Usuario usuario, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 
 		model.addAttribute("titulo", "Resutaldo form");
+		
+		if(result.hasErrors()) {
+			Map<String, String> errores = new HashMap();
+			result.getFieldErrors().forEach(error -> {
+				errores.put(error.getField(), "El campo "
+						.concat(error.getField())
+						.concat(" ")
+						.concat(error.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
+			return "form";
+		}
+
 		model.addAttribute("usuario", usuario);
 
 		return "resultado";
