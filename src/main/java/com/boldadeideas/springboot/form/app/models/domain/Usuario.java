3ea96d5e5a80e0a696ba085dc55b5654205fc92f
hecto <hecto@LAPTOP-QH1AMIIM>
2022-10-09 19:02:30 -0600

    Clase 66 Validacion de numero enteros con @Max @Min y NotNull

--- src/main/java/com/boldadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/boldadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,8 +1,11 @@
 package com.boldadeideas.springboot.form.app.models.domain;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.boldadeideas.springboot.form.app.validation.IdentificadorRegex;
@@ -25,6 +28,11 @@ public class Usuario {
 	@Requerido
 	@Email
 	private String email;
+	
+	@NotNull
+	@Min(5)
+	@Max(5000)
+	private Integer cuenta;
 
 	//@NotEmpty(message = "El nombre no puede ser vacio")
 	private String nombre;
@@ -79,6 +87,14 @@ public class Usuario {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Integer getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(Integer cuenta) {
+		this.cuenta = cuenta;
 	}
 
 }
