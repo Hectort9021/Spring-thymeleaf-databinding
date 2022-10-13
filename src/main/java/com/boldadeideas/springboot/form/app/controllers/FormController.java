5ff0306f6c7496fc5e21649c2f0b5208a6c79dc7
hecto <hecto@LAPTOP-QH1AMIIM>
2022-10-12 19:17:02 -0600

    Clase 79 - Llenando checkboxes con Map

--- src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
@@ -133,5 +133,16 @@ public class FormController {
 		
 		return roles;
 	}
+	
+	@ModelAttribute("listaRolesMap")
+	public Map<String, String> listaRolesMap(){
+		Map<String, String> roles = new HashMap<>();
+		roles.put("ROLE_ADMIN", "Administrador");
+		roles.put("ROLE_USER", "Usuario");
+		roles.put("ROLE_MODERATOR", "Moderador");
+		
+		return roles;
+		
+	}
 
 }
